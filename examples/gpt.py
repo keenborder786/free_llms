ede3dc9b66015d3d97430a3529c56d2de2422e6a
keenborder786 <21110290@lums.edu.pk>
2024-06-06 01:40:59 +0500

    [feat]: Examples Updated

--- examples/gpt.py
+++ examples/gpt.py
@@ -1,9 +1,12 @@
 from free_llms.models import GPTChrome
 driver_config = [] # pass in selnium driver config except for the following ["--disable-gpu", f"--window-size=1920,1080"]
 with GPTChrome(driver_config=driver_config,
-               email = 'email',
-               password = 'password') as session: # A single session started with ChartGPT
+               email = '21110290@lums.edu.pk',
+               password = 'password',
+               ) as session: # A single session started with ChartGPT
     data = session.send_prompt("""Write an SQL Query which shows how to get third highest salary
     """) # First Message
     data1 = session.send_prompt('Now convert it into python') # Second message
     print(session.messages) # Messages in the current session in pair of <Human,AI>
+
+
