afb15e5fe515a93c46ebd9a1c1b7d5fa5c16ab18
keenborder786 <21110290@lums.edu.pk>
2024-06-11 15:33:07 +0500

    [chore]: Added Example for Claude

---
+++ examples/claude.py
@@ -0,0 +1,10 @@
+from free_llms.models import ClaudeChrome
+driver_config = [] # pass in selnium driver config except for the following ["--disable-gpu", f"--window-size=1920,1080"]
+with ClaudeChrome(driver_config=driver_config,
+               email = 'mohammad.mohtashim78@gmail.com',
+               password = '', # password not needed for ClaudeChrome
+               ) as session: # A single session started with ClaudeChrome
+    # once you login, you will get a code at your email which you need to type in
+    session.send_prompt('What is silicon valley?')
+    session.send_prompt('How many seasons it had?')
+    print(session.messages)
