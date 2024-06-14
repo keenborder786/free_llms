8f478a041fcdd7d16dde5faa7d7d1ca3cde32d39
keenborder786 <21110290@lums.edu.pk>
2024-06-15 02:26:59 +0500

    [format]: Examples Formated

--- examples/langchain_model.py
+++ examples/langchain_model.py
@@ -1,11 +1,9 @@
 from free_llms.langchain_model import FreeLLMs
 from langchain.prompts import PromptTemplate
-# model name can be any of the following: 
-model = FreeLLMs(model_name = 'PreplexityChrome', llm_kwargs = {
-              'driver_config':[],
-               'email':'email',
-               'password':''})
 
-prompt = PromptTemplate.from_template('Write me a joke about {topic}')
-chain =  prompt | model | str
-print(chain.invoke({'topic':'coding'}))
+# model name can be any of the following:
+model = FreeLLMs(model_name="PreplexityChrome", llm_kwargs={"driver_config": [], "email": "email", "password": ""})
+
+prompt = PromptTemplate.from_template("Write me a joke about {topic}")
+chain = prompt | model | str
+print(chain.invoke({"topic": "coding"}))
--- examples/mistral.py
+++ examples/mistral.py
@@ -1,9 +1,11 @@
 from free_llms.models import MistralChrome
-driver_config = [] # pass in selnium driver config except for the following ["--disable-gpu", f"--window-size=1920,1080"]
-with MistralChrome(driver_config=driver_config,
-               email = '21110290@lums.edu.pk', # Mistral Email
-               password = '',# Mistral Password
-               ) as session: # A single session started with Mistral
-    session.send_prompt('Write a short long horro story of 100 woirds')
-    session.send_prompt('Make it funny')
+
+driver_config = []  # pass in selnium driver config except for the following ["--disable-gpu", f"--window-size=1920,1080"]
+with MistralChrome(
+    driver_config=driver_config,
+    email="21110290@lums.edu.pk",  # Mistral Email
+    password="",  # Mistral Password
+) as session:  # A single session started with Mistral
+    session.send_prompt("Write a short long horro story of 100 woirds")
+    session.send_prompt("Make it funny")
     print(session.messages)
